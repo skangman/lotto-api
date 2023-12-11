eb964bde435c2e5af61971029d6f06a6f7f9b297
skangman <kenjantha.dev@gmail.com>
2023-12-11 15:55:15 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000
+
+// var port = process.env.PORT || 9696;
+
+
+const api = require('./route/api');
+
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   next();
+})
+
+
+app.use(express.static('public'))
+
+app.use('/api', api);
+
+
+app.listen(port, () => {
+   console.log("application is listening on:", port);
+});
+
---
+++ route/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+const router = express.Router()
+const { LottoApi } = require('../controller/apiController')
+
+router.get('/', (req, res, next) => {
+    res.send("user api access");
+})
+
+
+router.get('/getLotto/:date', LottoApi);
+router.get('/getLotto', LottoApi);
+
+
+module.exports = router;
